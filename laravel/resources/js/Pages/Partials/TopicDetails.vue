afdb937850b66ec76632505a1f9b3b8924ae2690
Charles Chadwick <charles@mutuallyassured.com>
2024-09-16 09:34:40 -0700

    Created a TopicDetails component to handle the title and link.

---
+++ laravel/resources/js/Pages/Partials/TopicDetails.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+    topic: Object
+})
+</script>
+<template>
+    <p class="text-lg font-semibold text-gray-900">
+        <a :href="`discuss/${topic.id}`">{{ topic.title }}</a>
+    </p>
+</template>
